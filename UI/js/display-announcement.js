4a45b4cb1ab02ebaffdcbce3ec3c69285ffa9b37
de-scholar <descholar.stech@gmail.com>
2020-01-21 07:18:08 +0200

    bug(project): Fix almost the whole project issues
    -Fix index page
    -Fix Signup and Signin page
    -Refactor the css styles
    -Change the create to create announcement in navbar of a user
    -Add more flexibility to search bar (search by announcement name, status, id)
    -Make search bar place-holder visible
    -Fix create-announcement page
    -Restyle the table displaying list of all announcement
    -Fix the admin page
    -Make a single announcement clickable
    -Make user view details of an announcement whenever he/she clicks an announcement
    
    [Delivers#170797154]

--- UI/js/display-announcement.js
+++ UI/js/display-announcement.js
@@ -1,15 +1,14 @@
-import {announcements} from '../js/resources/announcements.js';
+import { announcements } from './resources/announcements.js';
 const displayAnnouncementsDiv = document.getElementById('announcements-display');
 
 const announcementsList = announcements.map((announc) => (
     `
-    <tr key=${announc.id}>
-        <td>${announc.id}</td>
-        <td>${announc.title}</td>
-        <td>${announc.startDate}</td>
-        <td>${announc.endDate}</td>
-        <td>${announc.state}</td>
-        <td>Update</td>
+    <tr id=${announc.id} key=${announc.id} single-result>
+        <td id=${announc.id}>${announc.id}</td>
+        <td id=${announc.id}>${announc.title}</td>
+        <td id=${announc.id}>${announc.startDate}</td>
+        <td id=${announc.id}>${announc.endDate}</td>
+        <td id=${announc.id}>${announc.status}</td>
     </tr>`
 ));
 
@@ -22,14 +21,15 @@ const displayAnnouncements = () => {
               <th>Name</th>
               <th>Valid from</th>
               <th>Valid until</th>
-              <th>State</th>
-              <th>Action</th>
+              <th>Status</th>
             </tr>
         </thead>
         <tbody>${announcementsList.join("")}</tbody>
     </table>
-    <div>
-          <button class="bg-dark width-100 hand-cursor">Click here load more ...</button>
+    <div class="container">
+          <button 
+          class="bg-dark width-20 margin-top-2 hand-cursor rounded-corner-15">
+          Click here to load more ...</button>
         </div>
     `;
 }
--- UI/js/search-announcement.js
+++ UI/js/search-announcement.js
@@ -6,42 +6,43 @@ const searchingFunc = (searchQuery) => {
     
     let matchings = announcements.filter((announc) => {
         const regex = new RegExp(`^${searchQuery}`, 'gi');
-        return announc.state.match(regex)||announc.id.match(regex);
+        return announc.status.match(regex) ||
+            announc.id.match(regex) ||
+            announc.title.match(regex) ||
+            announc.startDate.match(regex) ||
+            announc.endDate.match(regex);
     });
 
     let resultArr;
     if (matchings.length > 0) {
         resultArr = matchings.map((match) => (
     `
-    <tr key=${match.id}>
-        <td>${match.id}</td>
-        <td>${match.title}</td>
-        <td>${match.startDate}</td>
-        <td>${match.endDate}</td>
-        <td>${match.state}</td>
-        <td>Update</td>
+    <tr key=${match.id} id=${match.id} single-announcement>
+        <td id=${match.id}>${match.id}</td>
+        <td id=${match.id}>${match.title}</td>
+        <td id=${match.id}>${match.startDate}</td>
+        <td id=${match.id}>${match.endDate}</td>
+        <td id=${match.id}>${match.status}</td>
     </tr>`
         ));
     } else {
         resultArr = [`<tr>
-        <td>.</td>
+        <td></td>
         <td></td>
         <td><span class="text-danger">No result found</span></td>
         <td></td>
-        <td></td>
-        <td>Update</td>`];
+        <td></td>`];
     }
 
     displayAnnouncementsDiv.innerHTML = `
-    <table class="width-100 rounded-corner-15 list-of-announcements">
+    <table class="width-100 rounded-corner-15 list-of-announcements margin-bottom-5">
         <thead>
             <tr>
               <th>id</th>
               <th>Name</th>
               <th>Valid from</th>
               <th>Valid until</th>
-              <th>State</th>
-              <th>Action</th>
+              <th>status</th>
             </tr>
         </thead>
         <tbody>${resultArr.join("")}</tbody>
