d9ad5e445e926b249c955abba1ce26107f455cce
de-scholar <descholar.stech@gmail.com>
2020-01-24 01:16:34 +0200

    bug(implement-ttl-feedback):Implement all TTL feedbacks
     -use destructuring in userModels file
    -don't sent many unncesary info in token
    refactor the if statement in validatesignupdata file
    -add a default routes on the application
    
    [Delivers#170882598]

--- server/v1/routes/index.js
+++ server/v1/routes/index.js
@@ -1,4 +1,5 @@
 import userRouter from './api/user-routes';
 import announcementRouter from './api/announcement-routes';
+import defaltRoute from './api/default-routes';
 
-export default { userRouter, announcementRouter};
+export default { userRouter, announcementRouter,defaltRoute};
